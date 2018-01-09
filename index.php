ba6197f412ececa2c65463e01f7b17f2f92f70ae
Spiffomatic64 <root@spiffomatic64.com>
2018-01-09 16:59:26 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,173 @@
+<html>
+
+<body>
+<form action="./index.php" method="post">
+Instrument:
+    <select name="instrument">
+        <option value="1">Acoustic Grand Piano</option>
+        <option value="2">Bright Acoustic Piano</option>
+        <option value="3">Electric Grand Piano</option>
+        <option value="4">Honky-tonk Piano</option>
+        <option value="5">Electric Piano 1</option>
+        <option value="6">Electric Piano 2</option>
+        <option value="7">Harpsichord</option>
+        <option value="8">Clavi</option>
+        <option value="9">Celesta</option>
+        <option value="10">Glockenspiel</option>
+        <option value="11">Music Box</option>
+        <option value="12">Vibraphone</option>
+        <option value="13">Marimba</option>
+        <option value="14">Xylophone</option>
+        <option value="15">Tubular Bells</option>
+        <option value="16">Dulcimer</option>
+        <option value="17">Drawbar Organ</option>
+        <option value="18">Percussive Organ</option>
+        <option value="19" selected>Rock Organ</option>
+        <option value="20">Church Organ</option>
+        <option value="21">Reed Organ</option>
+        <option value="22">Accordion</option>
+        <option value="23">Harmonica</option>
+        <option value="24">Tango Accordion</option>
+        <option value="25">Acoustic Guitar (nylon)</option>
+        <option value="26">Acoustic Guitar (steel)</option>
+        <option value="27">Electric Guitar (jazz)</option>
+        <option value="28">Electric Guitar (clean)</option>
+        <option value="29">Electric Guitar (muted)</option>
+        <option value="30">Overdriven Guitar</option>
+        <option value="31">Distortion Guitar</option>
+        <option value="32">Guitar harmonics</option>
+        <option value="33">Acoustic Bass</option>
+        <option value="34">Electric Bass (finger)</option>
+        <option value="35">Electric Bass (pick)</option>
+        <option value="36">Fretless Bass</option>
+        <option value="37">Slap Bass 1</option>
+        <option value="38">Slap Bass 2</option>
+        <option value="39">Synth Bass 1</option>
+        <option value="40">Synth Bass 2</option>
+        <option value="41">Violin</option>
+        <option value="42">Viola</option>
+        <option value="43">Cello</option>
+        <option value="44">Contrabass</option>
+        <option value="45">Tremolo Strings</option>
+        <option value="46">Pizzicato Strings</option>
+        <option value="47">Orchestral Harp</option>
+        <option value="48">Timpani</option>
+        <option value="49">String Ensemble 1</option>
+        <option value="50">String Ensemble 2</option>
+        <option value="51">SynthStrings 1</option>
+        <option value="52">SynthStrings 2</option>
+        <option value="53">Choir Aahs</option>
+        <option value="54">Voice Oohs</option>
+        <option value="55">Synth Voice</option>
+        <option value="56">Orchestra Hit</option>
+        <option value="57">Trumpet</option>
+        <option value="58">Trombone</option>
+        <option value="59">Tuba</option>
+        <option value="60">Muted Trumpet</option>
+        <option value="61">French Horn</option>
+        <option value="62">Brass Section</option>
+        <option value="63">SynthBrass 1</option>
+        <option value="64">SynthBrass 2</option>
+        <option value="65">Soprano Sax</option>
+        <option value="66">Alto Sax</option>
+        <option value="67">Tenor Sax</option>
+        <option value="68">Baritone Sax</option>
+        <option value="69">Oboe</option>
+        <option value="70">English Horn</option>
+        <option value="71">Bassoon</option>
+        <option value="72">Clarinet</option>
+        <option value="73">Piccolo</option>
+        <option value="74">Flute</option>
+        <option value="75">Recorder</option>
+        <option value="76">Pan Flute</option>
+        <option value="77">Blown Bottle</option>
+        <option value="78">Shakuhachi</option>
+        <option value="79">Whistle</option>
+        <option value="80">Ocarina</option>
+        <option value="81">Lead 1 (square)</option>
+        <option value="82">Lead 2 (sawtooth)</option>
+        <option value="83">Lead 3 (calliope)</option>
+        <option value="84">Lead 4 (chiff)</option>
+        <option value="85">Lead 5 (charang)</option>
+        <option value="86">Lead 6 (voice)</option>
+        <option value="87">Lead 7 (fifths)</option>
+        <option value="88">Lead 8 (bass + lead)</option>
+        <option value="89">Pad 1 (new age)</option>
+        <option value="90">Pad 2 (warm)</option>
+        <option value="91">Pad 3 (polysynth)</option>
+        <option value="92">Pad 4 (choir)</option>
+        <option value="93">Pad 5 (bowed)</option>
+        <option value="94">Pad 6 (metallic)</option>
+        <option value="95">Pad 7 (halo)</option>
+        <option value="96">Pad 8 (sweep)</option>
+        <option value="97">FX 1 (rain)</option>
+        <option value="98">FX 2 (soundtrack)</option>
+        <option value="99">FX 3 (crystal)</option>
+        <option value="100">FX 4 (atmosphere)</option>
+        <option value="101">FX 5 (brightness)</option>
+        <option value="102">FX 6 (goblins)</option>
+        <option value="103">FX 7 (echoes)</option>
+        <option value="104">FX 8 (sci-fi)</option>
+        <option value="105">Sitar</option>
+        <option value="106">Banjo</option>
+        <option value="107">Shamisen</option>
+        <option value="108">Koto</option>
+        <option value="109">Kalimba</option>
+        <option value="110">Bag pipe</option>
+        <option value="111">Fiddle</option>
+        <option value="112">Shanai</option>
+        <option value="113">Tinkle Bell</option>
+        <option value="114">Agogo</option>
+        <option value="115">Steel Drums</option>
+        <option value="116">Woodblock</option>
+        <option value="117">Taiko Drum</option>
+        <option value="118">Melodic Tom</option>
+        <option value="119">Synth Drum</option>
+        <option value="120">Reverse Cymbal</option>
+        <option value="121">Guitar Fret Noise</option>
+        <option value="122">Breath Noise</option>
+        <option value="123">Seashore</option>
+        <option value="124">Bird Tweet</option>
+        <option value="125">Telephone Ring</option>
+        <option value="126">Helicopter</option>
+        <option value="127">Applause</option>
+        <option value="128">Gunshot</option>
+    </select>
+    <br>
+    Volume (0-127): <input type="text" name="volume" value="127"><br>
+    Key): <select name="key">
+            <option value="0" selected>A</option>
+            <option value="1">A&sharp;/B&flat;</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+            <option value="4">C&sharp;/D&flat;</option>
+            <option value="5">D</option>
+            <option value="6">D&sharp;/E&flat;</option>
+            <option value="7">E</option>
+            <option value="8">F</option>
+            <option value="9">F&sharp;/G&flat;</option>
+            <option value="10">G</option>
+            <option value="11">G&sharp;/A&flat;</option>            
+          </select>
+    <br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
+
+<?php
+
+    if(isset($_POST["instrument"]) && isset($_POST["volume"]) && isset($_POST["key"]))
+    {
+        $myfile = fopen("midi.txt", "w");
+        fwrite($myfile, $_POST["instrument"]);
+        fwrite($myfile, "\n");
+        fwrite($myfile, $_POST["volume"]);
+        fwrite($myfile, "\n");
+        fwrite($myfile, $_POST["key"]);
+        fclose($myfile);
+    }
+
+
+?>
